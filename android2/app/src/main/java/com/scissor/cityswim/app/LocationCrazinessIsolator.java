5941d7be9b9286314cbe3669187fffccabca6c89
William Pietri <william@scissor.com>
2015-04-13 08:09:16 -0700

    Adding location updates.

--- android2/app/src/main/java/com/scissor/cityswim/app/LocationCrazinessIsolator.java
+++ android2/app/src/main/java/com/scissor/cityswim/app/LocationCrazinessIsolator.java
@@ -1,11 +1,16 @@
 package com.scissor.cityswim.app;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.util.Log;
+import android.os.Bundle;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class LocationCrazinessIsolator {
     public static final int MAX_AGE_MS = 5 * 60 * 1000;
@@ -13,13 +18,16 @@ public class LocationCrazinessIsolator {
     private final LocationManager locationManager;
 
     public LocationCrazinessIsolator(Context context) {
-        locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        final Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        locationManager.requestLocationUpdates(30 * 1000, 100, criteria, new MyListener(), null);
 
     }
 
     public Location usefulRecentLocation() {
         Location best = bestRecentLocation();
-        if ((best != null)  && (best.getAccuracy() < MAX_ERROR_METERS)) {
+        if ((best != null) && (best.getAccuracy() < MAX_ERROR_METERS)) {
             return best;
         } else {
             return null;
@@ -29,14 +37,14 @@ public class LocationCrazinessIsolator {
     public Location bestRecentLocation() {
         List<String> matchingProviders = locationManager.getAllProviders();
         List<Location> recentLocations = new ArrayList<Location>();
-        for (String provider: matchingProviders) {
+        for (String provider : matchingProviders) {
             final Location location = locationManager.getLastKnownLocation(provider);
-            if(location != null && location.hasAccuracy() && location.getTime() > System.currentTimeMillis() - MAX_AGE_MS) {
+            if (location != null && location.hasAccuracy() && location.getTime() > System.currentTimeMillis() - MAX_AGE_MS) {
                 recentLocations.add(location);
             }
         }
         Collections.sort(recentLocations, new BestLocationAccuracy());
-        if (recentLocations.size()>0) {
+        if (recentLocations.size() > 0) {
             return recentLocations.get(0);
         } else {
             return null;
@@ -47,7 +55,34 @@ public class LocationCrazinessIsolator {
     private static class BestLocationAccuracy implements Comparator<Location> {
         @Override
         public int compare(Location a, Location b) {
-            return (int) ( 100 * (a.getAccuracy() - b.getAccuracy()));
+            return (int) (100 * (a.getAccuracy() - b.getAccuracy()));
+        }
+    }
+
+    /**
+     * Doesn't need to do anything because we don't need to respond actively to updates.
+     * The UI refreshes every 10 seconds and uses the best available location; that's plenty.
+     */
+    private class MyListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
         }
     }
 }
